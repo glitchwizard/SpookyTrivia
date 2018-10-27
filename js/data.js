349677299efffddad81c8393b9fbc6de603c3bef
warrioryoko <miangpellegri@gmail.com>
2018-10-27 16:58:17 -0700

    JS data files written

---
+++ js/data.js
@@ -0,0 +1,49 @@
+let dataTitle = [{
+    link: "https://www.imdb.com/title/tt0455857/",
+    title: "When a Stranger Calls",
+    id: 0
+},
+{
+    link: "https://www.imdb.com/title/tt0100814/",
+    title: "Tremors",
+    id: 1
+},
+{
+    link: "https://horrorfreaknews.com/thing-1982-review/3508",
+    title: "The Thing",
+    id: 2
+},
+{
+    link: "https://horrorfreaknews.com/sixth-sense-1999-review/3708",
+    title: "The Sixth Sense",
+    id: 3
+},
+{
+    link: "https://horrorfreaknews.com/silence-lambs-1991-review/3502", 
+    title: "The Silence of the Lambs",
+    id: 4
+},
+{
+    link: "https://horrorfreaknews.com/shining-1980-review/3422",
+    title: "The Shining",
+    id: 5
+},
+{
+    link: "https://horrorfreaknews.com/scream-1996-review/3880",
+    title: "Scream",
+    id: 6
+},
+{
+    link: "https://horrorfreaknews.com/saw-2004-review/3631",
+    title: "Saw",
+    id: 7
+},
+{
+    link: "https://horrorfreaknews.com/ring-2002-review/3786",
+    title: "The Ring",
+    id: 8
+},
+{
+    link: "https://horrorfreaknews.com/return-living-dead-1985-review/3703",
+    title: "The Return of the Living Dead"
+}];
---
+++ js/dataQuote.js
@@ -0,0 +1,40 @@
+let dataQuote = [{
+    quote: "You really scared me, if that’s what you wanted. Is that what you wanted?",
+    id: 0
+},
+{
+    quote: "Hey--check this out! I found the ass end!",
+    id: 1
+},
+{
+    quote: "Trust is a tough thing to come by these days.",
+    id: 2
+},
+{
+    quote: "I see dead people.",
+    id: 3
+},
+{
+    quote: "Who can say. Best thing for him, really. His therapy was going nowhere.",
+    id: 4
+},
+{
+    quote: "All work and no play makes Jack a dull boy.",
+    id: 5
+},
+{
+    quote: "No, please don’t kill me, Mr. Ghostface, I wanna be in the sequel!",
+    id: 6
+},
+{
+    quote: "I wanna play a game.",
+    id: 7
+},
+{
+    quote: "That was very student film.",
+    id: 8
+},
+{
+    quote: "I know you’re there Tina. Because I can smell your brains.",
+    id: 9
+}];
